697dd0aa57668fac224c09af186874a820725abc
ercan cimen <ercancimen3434@gmail.com>
2020-04-06 23:46:24 +0300

    firebase db

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require("firebase-admin");
 const express = require("express");
+const engines = require("consolidate");
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+)
+
+function getFacts(){
+    const ref = firebaseApp.database().ref('facts');
+    return ref.once('value').then(snap=>snap.val());
+}
 
 const app = express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs')
 
-app.get('/timestamp',(request,response)=>{
-    response.send(`${Date.now()}`);
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
 })
 
-app.get('/timestamp-cached',(request,response)=>{
+app.get('/',(request,response)=>{
     response.set('Cache-Control','public, max-age=300, s-maxage=600');
-    response.send(`${Date.now()}`);
+    getFacts().then(facts=>{
+        response.json(facts);
+    })
 })
 
 exports.app = functions.https.onRequest(app);
